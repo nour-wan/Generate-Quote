e388e82332f2951abc8265c6887b7b7e479af9d2
nour-wan <nourwanly77@gmail.com>
2023-01-26 14:27:19 -0800

    Generat Quote

--- main.js
+++ main.js
@@ -5,17 +5,20 @@ let saider=document.getElementById("saidBy");
 let mySpan=document.getElementById("qSpan");
 let myParagraph=document.getElementById("paragraph");
 
-var arrOfQuote=["Don't live the same year 75 times and call it a life.","It is hard to fail,but it is worse never to have triednto succeed.","Do it with passion or not at all."]
-var arrOfSaider=["Robin Sharma","Theodore Roosevet","Rosa Carey"]
+var arrOfQuote=["أَفْطَرَ الحَاجِمُ وَالمَحْجُومُ","اللَّهُمَّ إِنَّكَ سألْتَنا مِنْ أنْفُسِنا مَا لَا نَمْلِكُهُ إلَّا بِكَ،فأعْطنا مِنْكَ مَا يُرْضِيكَ عَنَّا","أُمِرْتُ أَن أُقاتلَ النَّاسَ حَتَّى يَشْهَدُوا أَن لَا إِلَه إلّا اللَّهُ وأنِّي رَسُولُ اللّهِ،فَإِذا قالُوها عَصَمُةا مِنِّي دِماءَهُمْ وأمْوَالهُمْ إلّا بِحَقِّها وحِسابُهُمْ على اللَّهِ","امْسَحُوا عَلى الْخُفَّيْنِ"]
+var arrOfSaider=["عن ثوبان","عن أبي هريرة","عن أبي هريرة","أحمد عن بلال"]
 
 var i=0;
 
 myBtn.onclick=function(){
+    
     myParagraph.textContent =arrOfQuote[i];
     saider.textContent=arrOfSaider[i];
     i++;
-    if (i==(arrOfQuote.length)){
+    if (i==(arrOfQuote.length)+1){
         i=0;
+        myParagraph.textContent =" أَبْرِدُوا بِالظُّهْرِ،فَِإِنَّ شِدَّةَ الحَرِّ مِنْ فَيْحِ جَهَنّمَ";
+        saider.textContent="عن المغيرة بن شعبة";
     };
     
 };
